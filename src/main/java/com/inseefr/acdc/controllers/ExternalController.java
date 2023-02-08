fb4471b3aaa8187348738c38935d56ff00b14183
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2023-02-08 11:00:16 +0100

    fix: copy-paste mistake

--- src/main/java/com/inseefr/acdc/controllers/ExternalController.java
+++ src/main/java/com/inseefr/acdc/controllers/ExternalController.java
@@ -8,22 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/external")
+@Tag(name="Campagnes (Data Collection) Controller")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
+@Slf4j
 public class ExternalController {
 
-    @RestController
-    @RequestMapping("/api/external")
-    @Tag(name="Campagnes (Data Collection) Controller")
-    @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
-    @Slf4j
-    public class DataCollectionController {
-        @Autowired
-        private ExternalService externalService;
+    @Autowired
+    private ExternalService externalService;
 
-        @Operation(summary = "Retrieve questionnaires from Pogues Back Office")
-        @GetMapping(value= "/pogues/questionnaire", produces = "application/json")
-        public ResponseEntity<String> getQuestionnairesFromPogues() {
-            log.info("Get questionnaires data from Pogues");
-            return ResponseEntity.ok(externalService.getQuestionnaires());
-        }
+    @Operation(summary = "Retrieve questionnaires from Pogues Back Office")
+    @GetMapping(value= "/pogues/questionnaire", produces = "application/json")
+    public ResponseEntity<String> getQuestionnairesFromPogues() {
+        log.info("Get questionnaires data from Pogues");
+        return ResponseEntity.ok(externalService.getQuestionnaires());
     }
 }
+
