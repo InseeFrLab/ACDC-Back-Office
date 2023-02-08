b4bf82c56263a073c532d68ee96549ba9c4b0012
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2023-02-08 10:53:17 +0100

    feat: retrieve questionnaire data from Pogues Back Office

---
+++ src/main/java/com/inseefr/acdc/controllers/ExternalController.java
@@ -0,0 +1,29 @@
+package com.inseefr.acdc.controllers;
+
+import com.inseefr.acdc.services.ExternalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class ExternalController {
+
+    @RestController
+    @RequestMapping("/api/external")
+    @Tag(name="Campagnes (Data Collection) Controller")
+    @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
+    @Slf4j
+    public class DataCollectionController {
+        @Autowired
+        private ExternalService externalService;
+
+        @Operation(summary = "Retrieve questionnaires from Pogues Back Office")
+        @GetMapping(value= "/pogues/questionnaire", produces = "application/json")
+        public ResponseEntity<String> getQuestionnairesFromPogues() {
+            log.info("Get questionnaires data from Pogues");
+            return ResponseEntity.ok(externalService.getQuestionnaires());
+        }
+    }
+}
---
+++ src/main/java/com/inseefr/acdc/services/ExternalService.java
@@ -0,0 +1,41 @@
+package com.inseefr.acdc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+@Slf4j
+public class ExternalService {
+    @Value("${fr.insee.pogues.uri}")
+    private String poguesUri;
+
+    public String getQuestionnaires(){
+
+        HttpClient client = HttpClient.newHttpClient();
+        String url = poguesUri + "api/persistence/questionnaires";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }
+        log.info("Response from Pogues: " + response.body());
+        return response.body();
+    }
+}
