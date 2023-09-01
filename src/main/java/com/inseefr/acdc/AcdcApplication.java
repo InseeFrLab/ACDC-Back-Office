f353c90c7cbcaa08627a711df94a5d87a434c3d2
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2023-09-01 10:18:21 +0200

    fix: added back Enq_LibelleEnquete pdf field

--- src/main/java/com/inseefr/acdc/AcdcApplication.java
+++ src/main/java/com/inseefr/acdc/AcdcApplication.java
@@ -2,9 +2,11 @@ package com.inseefr.acdc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableLoadTimeWeaving;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.inseefr.acdc"})
 public class AcdcApplication {
 
 	public static void main(String[] args) {
